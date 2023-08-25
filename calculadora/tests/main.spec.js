8ea5dadaff5209f19a7b3588aa1e82c976d59ca5
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-08-25 16:14:37 -0400

    convert es6 and babel

--- calculadora/tests/main.spec.js
+++ calculadora/tests/main.spec.js
@@ -1,54 +1,43 @@
-const calc = require('../src/main');
+import { sum, sub, mult, div } from '../src/main'
 
+describe('smoke tests', () => {
+  it('should exists the method "sum"', () => {
+    expect(sum).toBeInstanceOf(Function)
+  });
+  
+  it('should exists the method "sub"', () => {
+    expect(sub).toBeInstanceOf(Function)
+  });
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(soma(1, 2)).toBe(3);
-// });
+  it('should exists the method "mult"', () => {
+    expect(mult).toBeInstanceOf(Function)
+  });
 
+  it('should exists the method "div"', () => {
+    expect(div).toBeInstanceOf(Function)
+  });
 
-describe('calc', () => {
+})
 
-  describe('smoke tests', () => {
-    it('should exist the calc lib', () => {
-        expect(calc).toBeInstanceOf(Object)
-    });
-
-    it('should exists the method "sum"', () => {
-      expect(calc.sum).toBeInstanceOf(Function)
-    });
-    
-    it('should exists the method "sub"', () => {
-      expect(calc.sub).toBeInstanceOf(Function)
-    });
-
-    it('should exists the method "mult"', () => {
-      expect(calc.mult).toBeInstanceOf(Function)
-    });
-
-    it('should exists the method "div"', () => {
-      expect(calc.div).toBeInstanceOf(Function)
-    });
-
-  })
-
+describe('calculadora testes', () => {
   describe('Sum', () => {
     it('should return 4 when "sum(2, 2)"', () => {
-      expect(calc.sum(2, 2)).toEqual(4);
+      expect(sum(2, 2)).toEqual(4);
     });
   })
   describe('Sub', () => {
     it('should return 2 when "sub(4, 2)"', () => {
-      expect(calc.sub(4, 2)).toEqual(2);
+      expect(sub(4, 2)).toEqual(2);
     });
   })
   describe('Mult', () => {
     it('should return 10 when "mult(2, 5)"', () => {
-      expect(calc.mult(2, 5)).toEqual(10);
+      expect(mult(2, 5)).toEqual(10);
     });
   })
   describe('Div', () => {
     it('should return 3 when "div(6, 2)"', () => {
-      expect(calc.div(6, 2)).toEqual(3);
+      expect(div(6, 2)).toEqual(3);
     });
   })
 })
