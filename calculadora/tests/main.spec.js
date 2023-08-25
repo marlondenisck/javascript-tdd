b5acb9b2b70390f80ac190e816c63d19d9181a55
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-08-25 15:58:34 -0400

    calculadora p2

--- calculadora/tests/main.spec.js
+++ calculadora/tests/main.spec.js
@@ -30,4 +30,25 @@ describe('calc', () => {
     });
 
   })
+
+  describe('Sum', () => {
+    it('should return 4 when "sum(2, 2)"', () => {
+      expect(calc.sum(2, 2)).toEqual(4);
+    });
+  })
+  describe('Sub', () => {
+    it('should return 2 when "sub(4, 2)"', () => {
+      expect(calc.sub(4, 2)).toEqual(2);
+    });
+  })
+  describe('Mult', () => {
+    it('should return 10 when "mult(2, 5)"', () => {
+      expect(calc.mult(2, 5)).toEqual(10);
+    });
+  })
+  describe('Div', () => {
+    it('should return 3 when "div(6, 2)"', () => {
+      expect(calc.div(6, 2)).toEqual(3);
+    });
+  })
 })
