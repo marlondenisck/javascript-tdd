7e586d9f4b09578f89d03ae4b054c44333cfbf24
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-11-18 15:13:29 -0400

    Utilizando o Webpack para gerar a biblioteca em UMD

---
+++ spotify-wrapper/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'spotifyWrapper.js',
+    library: 'spotifyWrapper',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
